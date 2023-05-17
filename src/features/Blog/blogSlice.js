9579c3506639efae7372418510e5b371883405f6
Milli-saved <million12.tenkir@gmail.com>
2023-05-17 11:01:36 +0300

    Add cases for each functionality in the slice

--- src/features/Blog/blogSlice.js
+++ src/features/Blog/blogSlice.js
@@ -1,13 +1,42 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import BlogServices from "./blogServices"
+import BlogServices from "./blogServices";
+import { act } from "react-dom/test-utils";
 
 const initialState = {
   blogs: [],
+  blog: {},
   isLoading: false,
   isError: false,
   isSuccess: false,
   message: "",
 };
+
+export const getAllBlogs = createAsyncThunk("blog/getAllBlogs", async () => {
+  try {
+    return await BlogServices.getAllBlogs();
+  } catch (error) {
+    const message =
+      (error.response && error.response.data && error.response.data.message) ||
+      error.message ||
+      error.toString();
+  }
+});
+
+export const getOneBlog = createAsyncThunk(
+  "blog/getOneBlog",
+  async (blogId) => {
+    try {
+      return await BlogServices.getOneBlog(blogId);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+    }
+  }
+);
 
 export const blogSlice = createSlice({
   name: "blog",
@@ -15,7 +44,37 @@ export const blogSlice = createSlice({
   reducers: {
     reset: (state) => initialState,
   },
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder
+      // getAllBlogs
+      .addCase(getAllBlogs.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getAllBlogs.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.blogs = action.payload;
+      })
+      .addCase(getAllBlogs.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+      })
+      //getOneBlog
+      .addCase(getOneBlog.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getOneBlog.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.blog = action.payload;
+      })
+      .addCase(getOneBlog.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+      });
+  },
 });
 
 export const { reset } = blogSlice.actions;
