5a923f3e249365b56f27bd0f3ad7f13ca984d0b7
Milli-saved <million12.tenkir@gmail.com>
2023-05-18 10:14:14 +0300

    Add prettier and correct the outlines of the code.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Nav from './components/Nav';
-import Footer from './components/Footer';
-import HomePage from './Pages/HomePage';
-import About from './Pages/About';
-import Contact from './Pages/Contact';
-import SingleBlogPage from './Pages/SingleBlogPage';
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import HomePage from "./Pages/HomePage";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import SingleBlogPage from "./Pages/SingleBlogPage";
 
 function App() {
-    return (
-        <>
-            <Router>
-                <Nav />
-                <Routes>
-                    <Route path='/' element={<HomePage />} />
-                    <Route path='/blog/:blogId' element={<SingleBlogPage />} />
-                    <Route path='/about' element={<About />} />
-                    <Route path='/contact' element={<Contact />} />
-                </Routes>
-            </Router>
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/blog/:blogId" element={<SingleBlogPage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,20 +1,20 @@
-import Blog from './Blog';
+import Blog from "./Blog";
 
 const Blogs = () => {
-    return (
-        <div className='col-md-10 col-lg-8 col-xl-7'>
-            <Blog />
-            <Blog />
-            <Blog />
+  return (
+    <div className="col-md-10 col-lg-8 col-xl-7">
+      <Blog />
+      <Blog />
+      <Blog />
 
-            {/* <!-- Pager--> */}
-            <div className='d-flex justify-content-end mb-4'>
-                <a className='btn btn-primary text-uppercase' href='#!'>
-                    Older Posts →
-                </a>
-            </div>
-        </div>
-    );
+      {/* <!-- Pager--> */}
+      <div className="d-flex justify-content-end mb-4">
+        <a className="btn btn-primary text-uppercase" href="#!">
+          Older Posts →
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Blogs;
